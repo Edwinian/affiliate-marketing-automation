123ea4c1d8dab6135c7de9c6ae28ec6170187baa
Edwin Chan <eneon54321@gmail.com>
2025-07-31 15:36:46 +0800

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,7 @@
+import json
+
+
+def execute(event, context):
+    body = {"message": "Hello from AWS Lambda using Python!", "input": event}
+    response = {"statusCode": 200, "body": json.dumps(body)}
+    return response
