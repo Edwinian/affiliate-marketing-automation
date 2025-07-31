00b63d230ec3fa886aa61d49c55aa800cad88c79
Edwin Chan <eneon54321@gmail.com>
2025-07-31 15:49:23 +0800

    test deploy

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,6 @@ import json
 
 
 def execute(event, context):
-    body = {"message": "Hello from AWS Lambda using Python!", "input": event}
+    body = {"message": "Hello from AWS Lambda using Python", "input": event}
     response = {"statusCode": 200, "body": json.dumps(body)}
     return response
