a797a1c0250f7373159d677d4fa157894dc0f82b
Edwin Chan <eneon54321@gmail.com>
2025-08-23 16:44:47 +0800

    remove sentence_transformer

--- llm_service.py
+++ llm_service.py
@@ -1,5 +1,4 @@
 import os
-from sentence_transformers import SentenceTransformer, util
 from xai_sdk import Client
 from xai_sdk.chat import user
 from dotenv import load_dotenv
@@ -19,7 +18,6 @@ class LlmService:
     def __init__(self, model_name: str = "grok-3-mini"):
         self.model_name = model_name
         self.x_client = Client(api_key=os.getenv("XAI_API_KEY"))
-        self.sentence_transformer = SentenceTransformer("all-MiniLM-L6-v2")
 
     def generate_text(
         self,
@@ -37,13 +35,3 @@ class LlmService:
             return LlmErrorPrompt.LENGTH_EXCEEDED
 
         return response.content
-
-    def get_similarity_score(
-        self,
-        text1: str,
-        text2: str,
-    ) -> float:
-        embeddings1 = self.sentence_transformer.encode(text1, convert_to_tensor=True)
-        embeddings2 = self.sentence_transformer.encode(text2, convert_to_tensor=True)
-        similarity = util.cos_sim(embeddings1, embeddings2)[0][0]
-        return similarity
